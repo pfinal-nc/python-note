c4f7b97a1ee427449473519533ebefe11330db54
Q哥 <lampxiezi@163.com>
2017-12-04 21:08:57 +0800

    up

--- code/python-08.py
+++ code/python-08.py
@@ -3,8 +3,18 @@
 
 import sys
 
-print('命令行参数如下:')
-for i in sys.argv:
-   print(i)
 
-print('\n\nPython 路径为：', sys.path, '\n')
+# print('命令行参数如下:')
+# for i in sys.argv:
+#   print(i)
+
+# print('\n\nPython 路径为：', sys.path, '\n');
+
+def power(x,n):
+	s = 1;
+	while n>0:
+		n = n-1;
+		s = s*x;
+	return s;
+
+print(power(5,2));
