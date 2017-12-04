3ca52b84b1c0fc55cd5a8d3cada70aa170ffa1ef
Q哥 <lampxiezi@163.com>
2017-12-04 22:00:33 +0800

    up

--- code/python-08.py
+++ code/python-08.py
@@ -19,4 +19,53 @@ def power(x,n):
 
 print(power(5,2));
 
+# 函数的默认参数
+def enroll(name,gender,age=6,city='上海'):
+	print('name:',name);
+	print('gender',gender);
+	print('age',age);
+	print('city',city);
+# 给默认参数
+enroll('大爷','得的')
+# 给默认参数传递新的值
+enroll('大爷','得的',10)
+# 给后一个默认参数赋值
+enroll('大爷','得的',city='我了个去')
+
+def add_end(L=[]):
+    L.append('END')
+    for i in L:
+        print(i)
+    return L
+
+print(add_end([1]));
+
+
+# 可变参数的函数
+
+def calc(numbers):
+    sum=0
+    for n in numbers:
+        sum = sum + n * n
+    return sum
+
+total = calc([1,2,3,4])
+total1 = calc((1,2,3,4))
+print(total)
+print(total1)
+
+# 可变参数的函数
+
+def calc_a(*numbers):
+    sum = 0
+    for n in numbers:
+        sum = sum + n * n
+    return sum
+
+# 因为参数 是 *numbers 所以 可以传递任何参数
+total_01 = calc_a(1,2,3);
+total_02 = calc_a();
+print(total_01)
+print(total_02)
+
 
