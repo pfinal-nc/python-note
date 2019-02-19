f28d830ee9fe7d39b905f26bcb10c26f7ae7551f
南丞 <Lampxiezi@163.com>
2019-02-19 18:04:10 +0800

    更新

--- code/Python-hacker/hacker_information_collection.py
+++ code/Python-hacker/hacker_information_collection.py
@@ -5,7 +5,10 @@ import time
 from bs4 import BeautifulSoup
 import re
 import json
+import socket
+import urllib.request
 start = time.time()
+
 
 def chax():
     lid = input('请输入你要查询的域名:')
@@ -14,39 +17,48 @@ def chax():
     }
     pattern = re.compile(r'http://|https://')
     lid = re.sub(pattern, '', lid).rsplit('/')[0]
-    url = "http://site.ip138.com/domain/read.do?domain=" + format(lid)
-    urldomain = "http://site.ip138.com/{}/domain.htm".format(lid)
-    url2 = "http://site.ip138.com/{}/beian.htm".format(lid)
-    url3 = "http://site.ip138.com/{}/whois.htm".format(lid)
-    rb = requests.get(url, headers=head)
-    rb1 = requests.get(urldomain, headers=head)
-    rb2 = requests.get(url2, headers=head)
-    rb3 = requests.get(url3, headers=head)
-    # gf = BeautifulSoup(rb.content, 'html.parser')
+    try:
+        ip = socket.gethostbyname(lid)
+        print('[+]IP解析记录')
+        print('IP地址:%s' % ip)
+        # start = '/**/op_aladdin_callback('.__len__()
+        # jsonstr = json.loads(response[start:-2],encoding='gbk')
+        # location = jsonpath.jsonpath(jsonstr,'$..location')[0]
+        # print(location)
+    except:
+        print("this URL 2 IP ERROR ")
 
-    print('[+]IP解析记录')
-    gf = rb.content.decode()
-    dict_json = json.loads(gf)
-    for v in dict_json['data']:
-        print('\nIP:【'+ v['ip'] + '】\nSIGN:【' + v['sign'] + '】\n' )
-    gf1 = BeautifulSoup(rb1.content, 'html.parser')
-    print('[+]子域名查询')
-    for v in gf1.find_all('p'):
-         link2 = v.get_text()
-         print(link2)
-    gf2 = BeautifulSoup(rb2.content, 'html.parser')
-    print('[+]备案查询')
-    for s in gf2.find_all('p'):
-        link3 = s.get_text()
-        print(link3)
-    gf3 = BeautifulSoup(rb3.content, 'html.parser')
-    print('[+]whois查询')
-    for k in gf3.find_all('p'):
-        link4 = k.get_text()
-        print(link4)
+    url = 'https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?query={}&co=&resource_id=6006&ie=utf8&oe=gbk&cb=op_aladdin_callback&format=json'
+    req = urllib.request.Request(url=url.format(ip))
+    response = urllib.request.urlopen(req).read() 
+    start = '/**/op_aladdin_callback('.__len__()
+    ip_info = json.loads(response[start:-2].decode('gbk'));
+    print(ip_info['data'])
+    
+    # print('[+]IP解析记录')
+    # gf = rb.content.decode()
+    # dict_json = json.loads(gf)
+    # print(dict_json)
+    # for v in dict_json['data']:
+    #     print('\nIP:【'+ v['ip'] + '】\nSIGN:【' + v['sign'] + '】\n' )
+    # gf1 = BeautifulSoup(rb1.content, 'html.parser')
+    # print('[+]子域名查询')
+    # for v in gf1.find_all('p'):
+    #      link2 = v.get_text()
+    #      print(link2)
+    # gf2 = BeautifulSoup(rb2.content, 'html.parser')
+    # print('[+]备案查询')
+    # for s in gf2.find_all('p'):
+    #     link3 = s.get_text()
+    #     print(link3)
+    # gf3 = BeautifulSoup(rb3.content, 'html.parser')
+    # print('[+]whois查询')
+    # for k in gf3.find_all('p'):
+    #     link4 = k.get_text()
+    #     print(link4)
 
 
 if __name__ == "__main__":
     chax()
-    end=time.time()
-    print('查询耗时:',end - start)
+    end = time.time()
+    print('查询耗时:', end - start)
