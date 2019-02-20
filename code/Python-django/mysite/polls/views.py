e075cb35a83eef85dc88f88ad6aa8cef57e2c197
南丞 <Lampxiezi@163.com>
2019-02-20 15:52:27 +0800

    更新

--- code/Python-django/mysite/polls/views.py
+++ code/Python-django/mysite/polls/views.py
@@ -1,7 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Choice,Question
+from django.template import loader
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("前台首页")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Question.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('results', args=(question.id,)))
+    # return HttpResponse("You're voting on question %s." % question_id)
