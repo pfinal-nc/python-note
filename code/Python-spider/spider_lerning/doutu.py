d4b7bc9d5296508d1c643d33b41016d67b0461e4
南丞 <Lampxiezi@163.com>
2019-03-13 10:48:52 +0800

    更新

--- code/Python-spider/spider_lerning/doutu.py
+++ code/Python-spider/spider_lerning/doutu.py
@@ -1,7 +1,11 @@
 # -*- coding:utf-8 -*-
-import os
+import os,sys
 import urllib.request
 import re
+import time
+import threading
+import requests
+
 from bs4 import BeautifulSoup
 from lxml import etree
 
@@ -19,14 +23,32 @@ class Img:
         response = urllib.request.urlopen(request)
         return response
 
+    def get_img(self, html):
+        soup = etree.HTML(html)
+        items = soup.xpath('//div[@class="artile_des"]')
+        # print(items)
+        for item in items:
+            imgurl_list = item.xpath('table/tbody/tr/td/a/img/@onerror')
+            for i in imgurl_list:
+                th = threading.Thread(target=self.save_img, args=(i,))
+                th.start()
+
     def get_img_html(self, response):
         soup = BeautifulSoup(response, 'lxml')
         all_a = soup.find_all('a', class_='list-group-item')
         for i in all_a:
             # self.get_img(self.get_html(i['href']))
-            soup = BeautifulSoup(self.get_html(i['href']),'lxml')
-            print(soup)
+            content = self.get_html(i['href']).read().decode('utf-8')
+            self.get_img(content)
 
+    def save_img(self, img_url):
+        img_url = img_url.split('=')[-1][1:-2].replace('jp', 'jpg')
+        print(u'正在下载'+img_url)
+        img_content = requests.get(img_url).content
+        # print(img_content)
+        with open(os.path.dirname(sys.argv[0])+'/doutu/%s.jpg' % img_url.split('/')[-1], 'wb') as f:
+            f.write(img_content)
 if __name__ == "__main__":
-    dotu = Img(1)
-    dotu.get_img_html(dotu.get_html(dotu.siteUrl))
+    for i in range(1,11):
+        dotu = Img(i)
+        dotu.get_img_html(dotu.get_html(dotu.siteUrl))
