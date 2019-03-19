879a4afeb957b1645ba4021198d55412c0a912f8
南丞 <lampxiezi@163.com>
2019-03-19 21:28:26 +0800

    up

--- code/Python-hacker/url2ip.py
+++ code/Python-hacker/url2ip.py
@@ -7,39 +7,40 @@ import time
 import threading
 
 
-def get_ip(url):
-    try:
-        result = socket.gethostbyname_ex(socket.getfqdn(url))
-        print(result)
-    except:
-        # time.sleep(2)
-        pass
+def get_ip(urls):
+    for openurl in urls:
+        url = openurl.replace('http://', '')
+        try:
+            result = socket.gethostbyname_ex(socket.getfqdn(url))
+            print(result)
+        except:
+            # time.sleep(2)
+            pass
 
 
 def url2ip(urllist):
     threads = []
-    print(len(urllist))
-    # for openurl in urllist:
-    #     url = openurl.replace('http://', '')
-    #     t = threading.Thread(target=get_ip, args=(url,))
-    #     threads.append(t)
-    #     # print(socket.has_ipv6)
-    #     # url = openurl.replace('http://', '')
-    #     # # print(url)
-    # for t in threads:
-    #     t.setDaemon(True)
-    #     t.start()
+    num = round(len(urllist) / 500)
+    # print(num*500)
+    for i in range(1, num):
+        t = threading.Thread(target=get_ip, args=(urllist[0 * num * 500:500],))
+        threads.append(t)
+    # print(threads)
+    for t in threads:
+    #    t.setDaemon(True)
+        t.start()
 
 
 def get_url(file_url):
     # file_url = os.path.dirname(sys.argv[0]) + '/dna2.txt'
     # print(file_url)
-    urllist = open(file_url, "r+", encoding='utf-8')
-    # print(urllist)
-    url2ip(urllist)
+    urllist = open(file_url, "r", encoding='utf-8')
+    # print(urllist.read())
+    url2ip(urllist.readlines())
     # urllist.close()
 
 
 if __name__ == "__main__":
     file_url = os.path.dirname(sys.argv[0]) + '/school.txt'
+    # print(file_url)
     get_url(file_url)
