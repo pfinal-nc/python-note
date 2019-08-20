6e452846476e94d4b53a1050f780af7ceb67f1e6
南城 <lampxiezi@163.com>
2019-08-20 15:11:23 +0800

    up

---
+++ code/Python-tkinter/func/Func.py
@@ -0,0 +1,6 @@
+def json_view():
+    print('json_view')
+
+
+def md52():
+    print(md52)
--- code/Python-tkinter/tkinter1.py
+++ code/Python-tkinter/tkinter1.py
@@ -2,33 +2,43 @@ import tkinter as tk  # 导入tkinter模块
 from tkinter import messagebox
 from tkinter import simpledialog
 import config
+from func import Func
 
 
 class App:
     def __init__(self, master):
-        fm1 = tk.Frame(master)
-        menu = tk.Menu(fm1)
-        root.config(menu=menu)
-        # 设置菜单选项
-        # menu_one = self.get_menu(menu)
-        # menu.add_cascade(label='首页', menu=menu_one)
-        # self.get_menu(self, menu)
-        # menu.add_command(label='关于', command=self.about_me)
+        self.master = master
+        self.party = tk.StringVar()
+        self.init_menu()
 
-    def get_menu(self):
-        menu_list = config.MENU
-        print(menu_list)
+    def init_menu(self):
+        mBar = tk.Frame(self.master, relief=tk.RAISED, borderwidth=2)
+        mBar.pack(fill=tk.X)
+        self.rad_button(mBar)
 
-    def menu_func(self, params):
-        print(params)
+    def rad_button(self, mBar):
+        Radiobutton_button = tk.Menubutton(mBar, text='工具', underline=0)
+        Radiobutton_button.pack(side=tk.LEFT)
+        Radiobutton_button.menu = tk.Menu(Radiobutton_button)
+        for item in config.MENU:
+            for menu in item['工具']:
+                for k in menu:
+                    Radiobutton_button.menu.add('radiobutton',
+                                                {'label': k, 'variable': self.party, 'value': menu[k],'command':self.set_content})
 
-    def about_me(self):
-        tk.messagebox.showinfo('欢迎', "欢迎使用!\nBy PFinal")
+        Radiobutton_button['menu'] = Radiobutton_button.menu
+
+    def set_content(self):
+        print(self.party.get())
 
 
-root = tk.Tk()  # 主窗口
-root.title('吃饱喝足')  # 窗口标题
-root.geometry("800x800+200+50")
-display = App(root)
-display.get_menu()
+
+
+root = tk.Tk()
+root.title("开发小工具")
+root.geometry('600x400')
+# 禁止改变窗口大小
+# root.resizable(width=False, height=False)
+App(root)
+root.mainloop()
 # root.mainloop()
