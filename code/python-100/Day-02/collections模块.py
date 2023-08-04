0f6e89597fdf80294f5006e08d749e29d3cf2ec3
PFinal_南丞 <1370288056@qq.com>
2023-08-04 11:23:54 +0800

    2023-08-04 up

--- code/python-100/Day-02/collections模块.py
+++ code/python-100/Day-02/collections模块.py
@@ -3,8 +3,7 @@
 # @Author  : PFinal南丞 <lampxiezi@163.com
 # @File    : collections模块.py
 # @Software: PyCharm
-import queue
-from collections import namedtuple
+from collections import namedtuple, deque, defaultdict, Counter
 
 #  在内置数据类型（dict、list、set、tuple）的基础上，collections模块还提供了几个额外的数据类型：Counter、deque、defaultdict、namedtuple和OrderedDict等
 # 1.namedtuple 生成可以使用名字来访问元素内容的 tuple
@@ -56,18 +55,75 @@ Point = namedtuple("Point", ["x", "y"])
 # 10.queue.join() 实际上意味着等到队列为空， 再执行别的操作
 
 # 首先创建一个队列
-q = queue.Queue()
-# 往q 队列中依次放值
-q.put(9)
-q.put(8)
-q.put(7)
-# 往队列中依次取值
-print(q.get())
-print(q.get())
-print(q.get())
+# q = queue.Queue()
+# # 往q 队列中依次放值
+# q.put(9)
+# q.put(8)
+# q.put(7)
+# # 往队列中依次取值
+# print(q.get())
+# print(q.get())
+# print(q.get())
+#
+# #  如果不想出现阻塞 可以取值之前用 queue.qsize() 看看队列的大小
+# # 队列
+# q.put([1, 2, 3, 4, 5, 6, 7, 8])
+# q.put(8)
+# print(q)
 
-#  如果不想出现阻塞 可以取值之前用 queue.qsize() 看看队列的大小
-# 队列
-q.put([1, 2, 3, 4, 5, 6, 7, 8])
-q.put(8)
-print(q)
+# deque(双端队列)方法
+# deque() 创建一个空的双端队列
+# append() 向双端队列后面放数据
+# appendleft() 向双端队列 前面放数据
+# pop() 向双端队列后面取数据
+# popleft() 向双端队列前面取数据
+# add_front() 从队头加入一个item元素
+# add_rear() 从队尾加入一个item元素
+# remove_front() 从队头删除一个item元素
+# remove_rear()  从队尾删除一个item元素
+# is_empty() 判断双端队列是否为空
+# size() 返回队列的大小
+
+dq = deque([1, 2])
+
+# 向这个队列端插入 'a'
+dq.append('a')
+# 向这个队列前端插入‘b’
+dq.appendleft('b')
+# 双印这个双端队列
+print('插入数据后的队列', dq)
+
+# 向第二个位置插入数字 3
+dq.insert(2, 3)
+# 取数据
+print(dq.pop())
+print(dq.pop())
+print(dq.popleft())
+
+# 打印双端队列
+print(dq)
+
+# defaultdict 方法
+# defaultdict 接受一个工厂函数作为参数 dict = defaultdict(factory_function) factory_function  必须是可以调用的可以是list, set, str 等等
+# 作用当 key 不存在时，返回的是工厂哈拿书的默认值, defaultdict 是在操作字典中没有的键时, 会自动创建而不会报错, 且指定自动创建对应的值的默认类型
+
+values = [11, 22, 33, 44, 55, 66, 77, 88, 99, 90]
+
+my_dict = defaultdict(list)
+
+for value in values:
+    if value > 66:
+        my_dict['k1'].append(value)
+    else:
+        my_dict['k2'].append(value)
+
+print(my_dict)
+
+print(my_dict['k3'])
+
+# Counter 方法
+# counter  类的目的是用来跟踪值出现的次数, 它是一个无序的容器类型, 以字典的键值对形式存储, 其中元素作为 key, 其计数作为 value, 计数值可以是任意的 Interger
+# Counter类和其他语言的bags或multisets很相似。
+
+c = Counter('abcdeabcdabcaba')
+print(c)
