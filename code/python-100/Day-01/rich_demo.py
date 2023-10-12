14affffb4a94e8ad748a9b50e0b2ba6ab71b3dc0
PFinal_南丞 <1370288056@qq.com>
2023-10-12 11:18:21 +0800

    2023-10-11 更新

---
+++ code/python-100/Day-01/rich_demo.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/10/12 11:14
+# @Author  : PFinal南丞 <lampxiezi@163.com
+# @File    : rich_demo.py
+# @Software: PyCharm
+from rich import inspect
+
+lst_var = ['hello', 'world', 'pfinal', 'club']
+inspect(lst_var)
+# inspect(lst_var, methods=True)
