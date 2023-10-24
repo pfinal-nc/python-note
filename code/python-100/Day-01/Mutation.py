6695e993814e5cd285ff70c776f67eba9e4dfaf9
PFinal_南丞 <1370288056@qq.com>
2023-10-24 18:28:39 +0800

    2023-10-24 update

---
+++ code/python-100/Day-01/Mutation.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/10/24 16:57
+# @Author  : PFinal南丞 <lampxiezi@163.com
+# @File    : Mutation.py
+# @Software: PyCharm
+with open("demo.txt", "r") as f:
+    print(f.readlines())
