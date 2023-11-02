3ae6c6eded3533525bac62d5779b6e0c8f1b14a6
PFinal_南丞 <1370288056@qq.com>
2023-11-02 14:58:57 +0800

    2023-11-02 update

--- code/Python-mysql/mysql_option.py
+++
@@ -1,5 +0,0 @@
-from MysqlLet import MySQLet
-if __name__ == "__main__":
-    mysqlet = MySQLet(host = "127.0.0.1",user = "root", password= "root", database = "scrapy_ip")
-    data = mysqlet.query_formatrs('SELECT `ip` FROM proxy_ip');
-    print(data)
